113a5edfcc080f237d0d6234069dac1f3d6e598d
shunki23 <s2513511@u.tsukuba.ac.jp>
2025-06-11 14:06:18 +0900

    ヘッダーを作成

--- app/loan/display/page.tsx
+++ app/loan/display/page.tsx
@@ -1,7 +1,13 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+
 export default function Display() {
   return (
-  <div>
-    Hello World!
-  </div>
+    <div className="flex flex-col w-screen items-center justify-center">
+      <div className="text-2xl font-bold my-4">
+        貸し / 借り
+      </div>
+
+    </div>
   );
 }

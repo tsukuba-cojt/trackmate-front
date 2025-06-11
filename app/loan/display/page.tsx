e7d8c9ad769062e90dd7de59b3e0979fafea28e7
shunki23 <s2513511@u.tsukuba.ac.jp>
2025-06-11 14:29:59 +0900

    人と貸し借りの合計金額を表示するCardコンポーネントの作成

--- app/loan/display/page.tsx
+++ app/loan/display/page.tsx
@@ -2,12 +2,36 @@ import { Button } from "@/components/ui/button";
 import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 
 export default function Display() {
+
   return (
-    <div className="flex flex-col w-screen items-center justify-center">
+    <div className="flex flex-col w-full items-center justify-center">
       <div className="text-2xl font-bold my-4">
         貸し / 借り
       </div>
 
-    </div>
+      <div className="w-full flex flex-col justify-center items-center gap-4">
+        <Card className="w-3/4">
+          <CardContent className="">
+            <div>やすの</div>
+            <div className="flex justify-end text-4xl">¥ 1,500</div>
+          </CardContent>
+        </Card>
+
+        <Card className="w-3/4">
+          <CardContent className="">
+            <div>Astalum</div>
+            <div className="flex justify-end text-4xl">¥ 1,300</div>
+          </CardContent>
+        </Card>
+
+        <Card className="w-3/4">
+          <CardContent className="">
+            <div>こまつさん</div>
+            <div className="flex justify-end text-4xl">¥ 20,300</div>
+          </CardContent>
+        </Card>
+      </div>
+
+      </div>
   );
 }

14df5ef25738d12033e9186baa2d418f6251e3d4
shunki23 <s2513511@u.tsukuba.ac.jp>
2025-07-11 13:23:15 +0900

    貸し，借り状況を表示するCardの作成

--- app/loan/display/page.tsx
+++ app/loan/display/page.tsx
@@ -2,20 +2,41 @@
 
 import { Button } from "@/components/ui/button";
 import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+import { Car } from "lucide-react";
 import { useState } from "react";
 
-export default function Display() {
-  type LoanObject = {
+type LoanObject = {
     name: string, 
     sumLoan: number, 
-    key: number
-  }
+    isDebt: boolean, 
+    key: number, 
+}
 
+export default function Display() {
   const [loans, setLoans] = useState<LoanObject[]>([
-    {name: "やすの", sumLoan: 1500, key: 1}, 
-    {name: "Astalum", sumLoan: 1300, key: 2}, 
-    {name: "こまつさん", sumLoan: 20300, key: 3}
+    {
+      name: "やすの", 
+      sumLoan: 1500, 
+      isDebt: true,
+      key: 1, 
+    }, 
+    {
+      name: "Astalum", 
+      sumLoan: 1300, 
+      isDebt: false,
+      key: 2, 
+    }, 
+    {
+      name: "こまつさん", 
+      sumLoan: 20300, 
+      isDebt: false,
+      key: 3, 
+    }
   ])
+
+  const handleCardClick = (clickedKey: number) => {
+    
+  }
 
   return (
     <div className="flex flex-col w-full items-center justify-center">
@@ -23,13 +44,23 @@ export default function Display() {
         貸し / 借り
       </div>
 
-      <div className="w-full flex flex-col justify-center items-center gap-4">
+      <div className="flex w-full flex-col justify-center items-center gap-4">
         {loans.map((loan) => {
+          // 合計金額を表示
           return (
-            <Card className="w-3/4 rounded-3xl" key={loan.key}>
+            <Card 
+              className="w-3/4 rounded-3xl py-6"
+              key={loan.key}
+              onClick={() => handleCardClick(loan.key)}
+            >
               <CardContent>
-                <div>{loan.name}</div>
-                <div className="flex justify-end text-4xl">¥ {loan.sumLoan.toLocaleString()}</div>
+                <div className="font-bold">{loan.name}</div>
+                <div className="flex flex-row w-full items-center justify-between">
+                  <p className={loan.isDebt? "pl-2 font-bold text-2xl text-green-400": "pl-2 font-bold text-2xl text-red-400"}>
+                    {loan.isDebt? '貸し': '借り'}
+                  </p>
+                  <p className=" text-4xl">¥ {loan.sumLoan.toLocaleString()}</p>
+                </div>
               </CardContent>
             </Card>
           );

ef6c571852907979b730777e9af9cd6fc8cf8f2f
shunki23 <s2513511@u.tsukuba.ac.jp>
2025-07-14 11:01:23 +0900

    画面ui完成

---
+++ app/loan/register/page.tsx
@@ -0,0 +1,90 @@
+"use client"
+
+import { ChangeEvent, SetStateAction, useState } from "react";
+
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button";
+import TransitionButton from "@/components/transition";
+import { useRouter } from "next/navigation";
+
+type Person = {
+  id: string, 
+  name: string
+}
+
+export default function Register() {
+  const router = useRouter();
+
+  const [newPersonName, setNewPersonName] = useState<string>("");
+
+  const buttonStyle: string = "border-black text-2xl font-bold border-1 px-12 py-6 mb-20";
+
+  const [persons, setPersons] = useState<Person[]>([
+    {id: "1", name: "やす"}, 
+    {id: "2", name: "やすの"}, 
+    {id: "3", name: "小松さん"}, 
+  ])
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setNewPersonName(e.target.value);
+  }
+
+  const handleClickDeleteButton = () => {
+
+  }
+
+  const handleClickAddButton = () => {
+
+  }
+
+  const handleClickBackButton = () => {
+
+  }
+
+  return (
+    <div className="flex flex-col w-full min-h-screen items-center justify-center">
+      <div className="text-2xl font-bold mt-10">
+        貸した・借りた人編集
+      </div>
+
+      <div className="flex flex-col grow items-center gap-4 w-3/4 m-15">
+        <div className="flex w-full h-12 bg-white rounded-2xl items-center relative border border-dashed border-black">
+          <button 
+            className="w-8 h-8 bg-green-400 text-lg text-white font-semibold rounded-full absolute left-4" 
+            onClick={() => handleClickAddButton()}
+          >
+            ＋
+          </button>
+          <Input 
+            className="flex-grow text-center font-bold text-lg outline-none border-none h-full px-12"
+            placeholder="名前を入力"
+            onChange={(e) => handleChange(e)}
+          />
+        </div>
+
+        {
+          persons.map((person: Person) => {
+            return (
+              <div 
+              key={person.id}
+              className="flex w-full h-12 bg-gray-300 rounded-2xl items-center relative">
+                <button 
+                  className="w-8 h-8 bg-red-400 text-lg text-white font-semibold rounded-full absolute left-4"
+                  onClick={() => handleClickDeleteButton()}
+                >
+                  ー
+                </button>
+                <p className="flex-grow text-center font-bold text-lg px-12">
+                  {person.name}
+                </p>
+              </div>
+            );
+          })
+        }
+      </div>
+      <Button variant="outline" className={buttonStyle} onClick={() => handleClickBackButton()}>
+        戻る
+      </Button>
+    </div>
+  );
+}

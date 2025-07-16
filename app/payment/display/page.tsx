10e6e27f486c489a4f648a595fb3e11e048b1936
yasuno-N5j <yasuno@rokko-jitsugyo.co.jp>
2025-07-16 14:08:10 +0900

    いったん完成

--- app/payment/display/page.tsx
+++ app/payment/display/page.tsx
@@ -1,61 +1,66 @@
 "use client"
+import Switcher from "@/components/switcher";
 import { Card, CardContent, CardTitle } from "@/components/ui/card";
-import { useState } from "react";
+import { useRouter } from "next/navigation";
+import useSWR from "swr";
+
+const fetcher = (url: string) => fetch(url).then(res => res.json())
 
 export default function Display() {
-  const [activeTab, setActiveTab] = useState<"display" | "input">("display")
+  const { data, error, isLoading } = useSWR("http://localhost:8080/expenses", fetcher)
+
   return (
     <div className="flex flex-col w-full items-center">
       <div className="text-4xl font-bold my-4">
-        {activeTab === "display" ? "支出" : "入力"}
+        支出
       </div>
       {/* タブボタン */}
-      <div className="flex gap-10 mb-4">
-        <button className={`
-          ${activeTab === "display" ? "text-2xl border-b-4 border-black font-bold" : "text-2xl border-transparent text-gray-300"}`}
-          onClick={() => setActiveTab("display")}
-        >
-          支出
-        </button>
-        <button className={`
-          ${activeTab === "input" ? "text-2xl border-b-4 border-black font-bold" : "text-2xl border-transparent text-gray-300"}`}
-          onClick={() => setActiveTab("input")}
-        >
-          入力
-        </button>
+      <Switcher
+        leftText="表示"
+        leftLink="payment/display"
+        rightText="入力"
+        rightLink="/payment/input"
+        focus="left">
+      </Switcher>
+
+      {/* display内容はそのまま */}
+      <div className="w-4/5">
+        <div className="gap-4 w-full mb-4">
+          <Card className="w-full">
+            <CardTitle className="ml-3">使用金額</CardTitle>
+            <CardContent className="text-black font-bold text-3xl text-center">
+              {data.expenses_sum}
+            </CardContent>
+          </Card>
+        </div>
+
+        <div className="grid grid-cols-2 gap-4 w-full">
+          <Card>
+            <CardTitle className="ml-3">今月の予算</CardTitle>
+            <CardContent className="text-black font-bold text-3xl text-center">
+              {data.budget}
+            </CardContent>
+          </Card>
+          <Card>
+            <CardTitle className="ml-3">今月の残り額</CardTitle>
+            <CardContent className="text-black font-bold text-3xl text-center">
+              {data.remaining_budget}
+            </CardContent>
+          </Card>
+          <Card>
+            <CardTitle className="ml-3">負債</CardTitle>
+            <CardContent className="text-red-500 font-bold text-3xl text-center">
+              {data.debt_sum}
+            </CardContent>
+          </Card>
+          <Card>
+            <CardTitle className="ml-3">貸し</CardTitle>
+            <CardContent className="text-green-500 font-bold text-3xl text-center">
+              {data.loan_sum}
+            </CardContent>
+          </Card>
+        </div>
       </div>
-
-      {activeTab === "display" ? (
-        <div className="w-full">
-          <div className="gap-4 w-full px-4">
-            <Card className="w-full">
-              <CardTitle>使用金額</CardTitle>
-            </Card>
-          </div>
-
-          <div className="grid grid-cols-2 gap-4 w-full p-4">
-            <Card>
-              <CardTitle className="ml-3">今月の予算</CardTitle>
-            </Card>
-            <Card>
-              <CardTitle className="ml-3">今月の残り額</CardTitle>
-            </Card>
-            <Card>
-              <CardTitle className="ml-3">負債</CardTitle>
-              <CardContent className="text-red-500 font-bold text-3xl text-center">
-                1000円
-              </CardContent>
-            </Card>
-            <Card>
-              <CardTitle className="ml-3">貸し</CardTitle>
-            </Card>
-          </div>
-        </div>
-      ) : (
-        <div className="w-full px-4">
-          <h2 className="text-lg font-bold mb-2">金額の入力</h2>
-        </div>
-      )}
     </div>
   );
 }

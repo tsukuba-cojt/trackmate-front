410ec3c3291745319a331396c54881fca9403e8f
shunki23 <s2513511@u.tsukuba.ac.jp>
2025-07-16 10:28:13 +0900

    貸し借りの選択状況を示すstateを親コンポーネントへ移動 & Switcherコンポーネントの追加

--- app/loan/input/page.tsx
+++ app/loan/input/page.tsx
@@ -2,16 +2,32 @@
 
 import DebtRentToggleButton from "@/components/DebtRentToggleButton";
 import LoanInputForm from "@/components/LoanInputForm";
+import Switcher from "@/components/switcher";
+import { useState } from "react";
 
 export default function InputPage() {
+  const [isSelectedLend, setIsSelectedLend] = useState<boolean>(true);
+
   return (
-    <div className="flex flex-col w-full items-center justify-center gap-y-16 ">
+    <div className="flex flex-col w-full items-center justify-center">
       <div className="text-2xl font-bold my-4">
         貸し / 借り
       </div>
-
-      <DebtRentToggleButton></DebtRentToggleButton>
-      <LoanInputForm></LoanInputForm>
+      <Switcher
+      leftText="表示"
+      leftLink="loan/display.tsx"
+      rightText="入力"
+      rightLink="/loan/input.tsx"
+      focus="right"
+      ></Switcher>
+      <DebtRentToggleButton
+      isSelectedLend={isSelectedLend}
+      setIsSelectedLend={setIsSelectedLend}
+      className="mb-15"
+      ></DebtRentToggleButton>
+      <LoanInputForm
+      
+      ></LoanInputForm>
     </div>
   );
 }

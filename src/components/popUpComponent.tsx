380de6538fe27ccd443b91ac3e34bd093d348355
shunki23 <s2513511@u.tsukuba.ac.jp>
2025-07-25 14:25:48 +0900

    PopUpComponentの作成

---
+++ src/components/popUpComponent.tsx
@@ -0,0 +1,34 @@
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogOverlay,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+
+type Props = {
+  isOpen: boolean,
+  onClose: () => void,
+  errorMessage: string, 
+  imgPath: string
+}
+
+export function PopUpComponent({isOpen, onClose, errorMessage, imgPath}: Props) {
+  return (
+    <Dialog open={isOpen} onOpenChange={onClose}>
+      <DialogContent className="sm:max-w-md bg-white" aria-describedby={undefined} showCloseButton={false}>
+        <DialogHeader>
+          <DialogTitle>{errorMessage}</DialogTitle>
+        </DialogHeader>
+        <DialogDescription></DialogDescription>
+        <div className="flex justify-center items-center gap-2">
+          <img src={imgPath} alt="" className="w-1/4"/>
+        </div>
+      </DialogContent>
+    </Dialog>
+  )
+}
